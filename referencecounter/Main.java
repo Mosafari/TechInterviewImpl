9431238c39c86f33e0a6ddc9b8bc812228a172a0
mosafari <m.safari2680@gmail.com>
2023-10-11 20:02:44 +0330

    # when use runable + lambda #dosen't work properly

--- referencecounter/Main.java
+++ referencecounter/Main.java
@@ -25,7 +25,33 @@ public class Main {
         coffeeShop2.gc();
         System.out.println(coffeeShop.getCount());
         
-        Thread t1 = new Thread(() -> {
+        // Thread t1 = new Thread(() -> {
+        //     coffeeShop.addReference();
+        //     System.out.println(coffeeShop.getCount());
+        //     try {
+        //         Thread.sleep(3000);
+        //         coffeeShop.setSells("caramel", 500);
+        //         System.out.println(coffeeShop.getSells());
+        //         System.out.println(coffeeShop.sellsOf("caramel")+" T1");
+        //         } catch (InterruptedException e) {
+        //                 e.printStackTrace();
+        //             }
+        //         });
+                
+        //         Thread t2 = new Thread(() -> {
+        //             coffeeShop.addReference();
+        //             System.out.println(coffeeShop.getCount());
+        //             try {
+        //                 Thread.sleep(2000);
+        //                 coffeeShop.setSells("caramel", 50);
+        //                 System.out.println(coffeeShop.getSells());
+        //                 System.out.println(coffeeShop.sellsOf("caramel")+" T2");
+        //     } catch (InterruptedException e) {
+        //         e.printStackTrace();
+        //     }
+        // });
+        // create new runable
+        Runnable task = () -> {
             coffeeShop.addReference();
             System.out.println(coffeeShop.getCount());
             try {
@@ -36,22 +62,26 @@ public class Main {
                 } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                };
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+        Thread t3 = new Thread(() -> {
+            coffeeShop.addReference();
+            System.out.println(coffeeShop.getCount());
+            try {
+                Thread.sleep(1000);
+                coffeeShop.setSells("caramel", -200);
+                System.out.println(coffeeShop.getSells());
+                System.out.println(coffeeShop.sellsOf("caramel")+" T1");
+                } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 });
-                
-                Thread t2 = new Thread(() -> {
-                    coffeeShop.addReference();
-                    System.out.println(coffeeShop.getCount());
-                    try {
-                        Thread.sleep(2000);
-                        coffeeShop.setSells("caramel", 50);
-                        System.out.println(coffeeShop.getSells());
-                        System.out.println(coffeeShop.sellsOf("caramel")+" T2");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+
+
         t1.start();
         t2.start();
+        t3.start();
         // t1.join();
         // t2.join();
     }
